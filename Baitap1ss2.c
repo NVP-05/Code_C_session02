cc9459d8d1c181ab8d2d2e253a0385ad9fdaa453
NVP-05 <147390576+NVP-05@users.noreply.github.com>
2023-11-07 22:56:34 +0700

    Add files via upload

---
+++ Baitap1ss2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int edge = 4;
+	int long_edge = 5;
+	int short_edge = 3;
+    const float PI = 3.14;
+    int radius = 3;
+    int perimeter_vuong = edge * 4;
+    int erimeter_chunhat = (long_edge+short_edge)*2;
+    int perimeter_tron = 2*PI* radius;
+    int acreage_vuong = edge*edge;
+    int acreage_chunhat = long_edge * short_edge;
+    int acreage_tron = PI*radius*radius;
+}
+
